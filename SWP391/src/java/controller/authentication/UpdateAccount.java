0cff9e2708785f5e9a26e81035dde6494f26554f
toanpv224 <toanpv224@gmail.com>
2022-06-15 17:19:10 +0700

    Update Iterration 2 Pham Van Toan 15/06/22

---
+++ SWP391/src/java/controller/authentication/UpdateAccount.java
@@ -0,0 +1,186 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.authentication;
+
+import dal.AccountDAO;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Random;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
+import model.Account;
+
+/**
+ *
+ * @author tretr
+ */
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 2,
+        maxFileSize = 1024 * 1024 * 50,
+        maxRequestSize = 1024 * 1024 * 50)
+public class UpdateAccount extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+
+        String id = request.getParameter("id");
+        String fname = request.getParameter("fname");
+        String phone = request.getParameter("phone");
+        String gender = request.getParameter("gender");
+        String city = request.getParameter("city");
+        String country = request.getParameter("country");
+        String address = request.getParameter("address");
+
+        String type = request.getParameter("type");
+
+        AccountDAO dao = new AccountDAO();
+        Account a = new Account();
+        HttpSession session = request.getSession();
+
+        if (type.equals("1")) {
+
+            // Create path components to save the file
+            final String path = getFolderUploadPath();
+            final Part filePart = request.getPart("file");
+            String fileName = getFileName(filePart);
+            Random r = new Random();
+
+            if (!fileName.equals("")) {
+                while (dao.checkAccountImageExist(fileName)) {
+                    fileName = r.nextInt(10) + fileName;
+                }
+                fileName = "images\\account-images\\" + fileName;
+                OutputStream out = null;
+                InputStream filecontent = null;
+                final PrintWriter writer = response.getWriter();
+
+                try {
+                    File f = new File(path + fileName);
+                    System.out.println(path + fileName);
+                    if (f.exists()) {
+                        writer.println("File " + fileName + " already exist at " + path);
+                    } else {
+                        out = new FileOutputStream(f);
+                        filecontent = filePart.getInputStream();
+
+                        int read = 0;
+                        final byte[] bytes = new byte[1024];
+
+                        while ((read = filecontent.read(bytes)) != -1) {
+                            out.write(bytes, 0, read);
+                        }
+                    }
+                } catch (FileNotFoundException fne) {
+                    writer.println("<br/> ERROR: " + fne.getMessage());
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+
+            a.setUser_id(Integer.parseInt(id));
+            a.setImage_url(fileName);
+
+            dao.updateAccountImg(a);
+        } else {
+
+            a.setUser_id(Integer.parseInt(id));
+            a.setFull_name(fname);
+            a.setPhone(phone);
+            a.setGender(Boolean.getBoolean(gender));
+            a.setCity(city);
+            a.setCountry(country);
+            a.setAddress(address);
+            dao.updateAccount(a);
+        }
+
+        a = (Account) request.getSession().getAttribute("account");
+
+        a = dao.getAccountByID(a);
+        session.setAttribute("account", a);
+        request.getRequestDispatcher("home").forward(request, response);
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    private String getFileName(final Part part) {
+        for (String content : part.getHeader("content-disposition").split(";")) {
+            if (content.trim().startsWith("filename")) {
+                return content.substring(
+                        content.indexOf('=') + 1).trim().replace("\"", "");
+            }
+        }
+        return null;
+    }
+
+    public String getFolderUploadPath() {
+        String path = getServletContext().getRealPath("/").replace("\\build", "");
+        File folderUpload = new File(path);
+        if (!folderUpload.exists()) {
+            folderUpload.mkdirs();
+        }
+        return path;
+    }
+
+}
--- SWP391/src/java/dal/AccountDAO.java
+++ SWP391/src/java/dal/AccountDAO.java
@@ -10,7 +10,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletContext;
@@ -32,6 +31,27 @@ public class AccountDAO extends DBContext {
             PreparedStatement stm = connection.prepareStatement(sql);
             stm.setString(1, username);
             stm.setString(2, password);
+            ResultSet rs = stm.executeQuery();
+            if (rs.next()) {
+                Account account = new Account(rs.getInt("user_id"), rs.getString("username"), rs.getString("password"),
+                        rs.getString("full_name"), rs.getInt("role_id"), rs.getBoolean("gender"), rs.getString("email"),
+                        rs.getString("city"), rs.getString("country"), rs.getString("address"), rs.getString("phone"),
+                        rs.getString("image_url"), rs.getBoolean("featured"), rs.getString("hash"), rs.getInt("active"));
+                return account;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AccountDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public Account getAccountByID(Account acc) {
+        try {
+            String sql = "SELECT *"
+                    + "  FROM [accounts]\n"
+                    + "  WHERE [user_id] = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, acc.getUser_id());
             ResultSet rs = stm.executeQuery();
             if (rs.next()) {
                 Account account = new Account(rs.getInt("user_id"), rs.getString("username"), rs.getString("password"),
@@ -97,6 +117,22 @@ public class AccountDAO extends DBContext {
         return null;
     }
 
+    public boolean checkAccountImageExist(String img) {
+        String sql = "SELECT [image_url]\n"
+                + "  FROM [accounts]\n"
+                + "  WHERE [image_url] = ?";
+        try {
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setString(1, "images\\account-images\\" + img);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                return true;
+            }
+        } catch (SQLException e) {
+        }
+        return false;
+    }
+
     public String singup(Account acc) {
         String sql = "INSERT INTO [dbo].[accounts]\n"
                 + "           ([username]\n"
@@ -107,9 +143,12 @@ public class AccountDAO extends DBContext {
                 + "           ,[phone]\n"
                 + "           ,[address]\n"
                 + "           ,[hash]\n"
+                + "           ,[image_url]\n"
+                + "           ,[city]\n"
+                + "           ,[country]\n"
                 + "           ,[active])\n"
                 + "     VALUES\n"
-                + "           (?,?,?,?,?,?,?,?,'0')";
+                + "           (?,?,?,?,?,?,?,?,'images\\account-images\\acc.png','(Nah)','(Nah)','0')";
         try {
             PreparedStatement stm = connection.prepareStatement(sql);
             stm.setString(1, acc.getUsername());
@@ -128,6 +167,44 @@ public class AccountDAO extends DBContext {
         } catch (Exception e) {
         }
         return "Success";
+    }
+
+    public void updateAccount(Account a) {
+        String sql = "UPDATE [accounts]\n"
+                + "   SET [full_name] = ?\n"
+                + "      ,[gender] = ?\n"
+                + "      ,[city] = ?\n"
+                + "      ,[country] = ?\n"
+                + "      ,[address] = ?\n"
+                + "      ,[phone] = ?\n"
+                + " WHERE [user_id] = ?";
+        try {
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setString(1, a.getFull_name());
+            stm.setBoolean(2, a.isGender());
+            stm.setString(3, a.getCity());
+            stm.setString(4, a.getCountry());
+            stm.setString(5, a.getAddress());
+            stm.setString(6, a.getPhone());
+            stm.setInt(7, a.getUser_id());
+
+            ResultSet rs = stm.executeQuery();
+        } catch (SQLException e) {
+        }
+    }
+
+    public void updateAccountImg(Account a) {
+        String sql = "UPDATE [accounts]\n"
+                + "   SET [image_url] = ?\n"
+                + " WHERE [user_id] = ?";
+        try {
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setString(1, a.getImage_url());
+            stm.setInt(2, a.getUser_id());
+
+            ResultSet rs = stm.executeQuery();
+        } catch (SQLException e) {
+        }
     }
 
     public void updateHashCode(Account acc) {
@@ -155,7 +232,7 @@ public class AccountDAO extends DBContext {
         } catch (SQLException e) {
         }
     }
-    
+
     public void updatePassword(String mail, String pass) {
         String sql = "UPDATE [accounts]\n"
                 + "   SET [password] = ?\n"
@@ -168,8 +245,8 @@ public class AccountDAO extends DBContext {
         } catch (SQLException e) {
         }
     }
-    
-        public int getTotalCustomers() {
+
+    public int getTotalCustomers() {
         String sql = "select count(user_id) from accounts";
         try {
             PreparedStatement st = connection.prepareStatement(sql);
@@ -182,7 +259,8 @@ public class AccountDAO extends DBContext {
         }
         return 0;
     }
-        public int getNumberOfRegisteredCustomerByDay(LocalDate start) {
+
+    public int getNumberOfRegisteredCustomerByDay(LocalDate start) {
         String sql = "select COUNT(user_id) from accounts where registered_date between ? and ?";
         try {
             PreparedStatement st = connection.prepareStatement(sql);
@@ -197,10 +275,12 @@ public class AccountDAO extends DBContext {
         }
         return 0;
     }
+
     public static void main(String[] args) {
         AccountDAO adb = new AccountDAO();
-        LocalDate start = LocalDate.parse("2022-06-12",DateTimeFormatter.ISO_LOCAL_DATE );
-        System.out.println(adb.getNumberOfRegisteredCustomerByDay(start));
+        Account a = new Account(7, "toanpv123", "123", "pham toan", 0, true, "123@123", "hanoi", "vn", "hd - hn", "09999999", "tt", true);
+        adb.singup(a);
+        System.out.println(a.getEmail());
     }
 
 }
